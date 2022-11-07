601edc35917bd3351da4c4b6e7de1a8bf2aeb5db
idungstanley <sundaystanley56@gmail.com>
2022-11-07 07:25:16 +0100

    add javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+var ourArray = ["ransom", "is", "rugged"];
+ourArray.push(["rugged", "for", "christ" ]);
+console.log(ourArray);
+
+var myArray = [["ransom",7], ["chisom", 8]];
+var removedFromMyArray = myArray.shift();
+console.log(myArray);
+
+var hisArray = [[ "Herryayo"   , "had" , "a"   , "scary" ]];
+hisArray.push(["dream"]);
+console.log(hisArray);
+
+var hisArray = [[ "Herryayo"   , "had" , "a"   , "scary" ], ["God strengthen my heart"]];
+var removedFromHisArray = hisArray.pop();
+console.log(hisArray);
+
+var hisArray = [[ "Herryayo"   , "had" , "a"   , "scary" ], ["God strengthen my heart"]];
+var removedFromHisArray = hisArray.shift();
+console.log(hisArray);
+
+var hisArray = [[ "Herryayo"   , "had" , "a"   , "scary" ], ["God strengthen my heart"]];
+var removedFromHisArray = hisArray.pop();
+ hisArray.unshift("my one and only");
+ hisArray.push("father help me bless her")
+console.log(hisArray);
+
+var myList = [["Cereals", 3] , ["milk", 4], ["banana", 3 ], ["juice", 7] ,  ["eggs", 8] ];
+console.log(myList)
