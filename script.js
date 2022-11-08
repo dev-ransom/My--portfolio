fc279f1a2d7e413e834b9e4e8c315392d594f4b6
idungstanley <sundaystanley56@gmail.com>
2022-11-08 18:21:14 +0100

    add updates on javascript

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-var ourArray = ["ransom", "is", "rugged"];
-ourArray.push(["rugged", "for", "christ" ]);
-console.log(ourArray);
+function ourReusableFunction() {
 
-var myArray = [["ransom",7], ["chisom", 8]];
-var removedFromMyArray = myArray.shift();
-console.log(myArray);
+console.log("God your word is true")
 
-var hisArray = [[ "Herryayo"   , "had" , "a"   , "scary" ]];
-hisArray.push(["dream"]);
-console.log(hisArray);
+}
 
-var hisArray = [[ "Herryayo"   , "had" , "a"   , "scary" ], ["God strengthen my heart"]];
-var removedFromHisArray = hisArray.pop();
-console.log(hisArray);
+ourReusableFunction();
+ 
+function resuableFunction() {
 
-var hisArray = [[ "Herryayo"   , "had" , "a"   , "scary" ], ["God strengthen my heart"]];
-var removedFromHisArray = hisArray.shift();
-console.log(hisArray);
+console.log("God is the greatest, the author and the finisher of my faith");
 
-var hisArray = [[ "Herryayo"   , "had" , "a"   , "scary" ], ["God strengthen my heart"]];
-var removedFromHisArray = hisArray.pop();
- hisArray.unshift("my one and only");
- hisArray.push("father help me bless her")
-console.log(hisArray);
+}
 
-var myList = [["Cereals", 3] , ["milk", 4], ["banana", 3 ], ["juice", 7] ,  ["eggs", 8] ];
-console.log(myList)
+resuableFunction();
+
+
+function myReusableFunction( ) {
+
+console.log("my daughter has a great future")
+
+
+}
+
+myReusableFunction( );
